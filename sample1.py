f23f8fbca46c380316c9ac52780027b32cd2b727
Farheena-NRIDegree <farheenashaik32@gmail.com>
2025-01-23 18:46:22 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,15 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import time
+>>> def ask_question(
+KeyboardInterrupt
+>>> 
+=============================================== RESTART: C:/Users/User/OneDrive/Desktop/Farheena/sample.py ==============================================
+What is the capital of France?
+1. Paris
+2. London
+3. Berlin
+4. Madrid
+Enter the number of your choice: 1
+Correct!
+Your final score is 10/100
